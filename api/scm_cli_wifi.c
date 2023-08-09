4813c231a98403424cc4e2237b2914f55a0704ab
Jesse-Xu615 <jesse.hsu@senscomm.com>
2023-08-09 16:10:43 +0800

    [module] test 3
    New features:
    
    Changes:
    
    Fixes:
    
    Issues:

--- api/scm_cli_wifi.c
+++ api/scm_cli_wifi.c
@@ -9,7 +9,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/*  test 2 */
+/*  test 3 */
 #include <hal/kernel.h>
 #include <hal/wlan.h>
 #include <hal/kmem.h>
